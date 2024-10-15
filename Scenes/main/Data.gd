800aca287da3fc9d7f31cf965e8fc22f172763b1
JerryShen <jerry82422@gmail.com>
2024-10-15 19:49:23 +0800

    Update map1 reference

--- Scenes/main/Data.gd
+++ Scenes/main/Data.gd
@@ -154,7 +154,7 @@ const enemies := {
 const maps := {
 	"map1": {
 		"name": "Grass Map",
-		"bg": "res://Assets/maps/map1.webp",
+		"bg": "res://Assets/maps/map1.png",
 		"scene": "res://Scenes/maps/map1.tscn",
 		"baseHp": 10,
 		"startingGold": 100,
